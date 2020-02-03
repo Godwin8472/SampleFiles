96db537fa600146cc43447f18a82e4d97e435407
FELICIA <wingod8472@gmail.com>
2020-02-03 11:22:07 +0530

    Files Added

---
+++ seleinum/src/com/seleinum/ak/Day1prac.java
@@ -0,0 +1,18 @@
+package com.seleinum.ak;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1prac {
+	public static void main(String[] args) throws Throwable{
+		System.setProperty("webDriver.chrome.driver","C:\\Users\\personal work\\eclipse-workspace\\seleinum\\Driver\\chromedriver.exe");
+	
+	WebDriver driver =new ChromeDriver();
+//	driver.get("https://www.facebook.com");
+//	driver.manage().window().maximize();
+	}
+	
+
+}
+
+
